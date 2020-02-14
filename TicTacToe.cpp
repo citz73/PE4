8c569cabfb4d1fe96eebce6074b9c79d2b113985
citz73 <citz73@gmail.com>
2020-02-13 20:50:41 -0700

    merge conflict

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -6,8 +6,20 @@ std::vector<std::vector<int>> createBoard() {
     std::vector<std::vector<int>> board {{0,0,0}, {0,0,0}, {0,0,0}};
     return board;
 }
+void DisplayBoard(std::vector<std::vector<int>> board) {
+    int boardsize = board.size();
+    std::cout << boardsize << std::endl;
+    for(int i=0; i <boardsize; i++) {
+    for(int j=0; j<boardsize; j++){
+    std::cout<<board[i][j];
+    }
+    std::cout<<"\n";
+    }
+}
 
 
 int main() {
     std::vector<std::vector<int>> board = createBoard();
+    DisplayBoard(board);
+    
 }
